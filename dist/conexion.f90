479a63a80d76010f8fbb7713a1dc4f33b62c8a24
C-arl-os <112126461+C-arl-os@users.noreply.github.com>
2024-09-22 17:26:35 -0600

    nm

--- dist/conexion.f90
+++
@@ -1,8 +0,0 @@
-program procesar_datos
-    implicit none
-    character(len=100) :: entrada
-    read(*, '(A)') entrada
-    entrada = trim(entrada) //  char(10) // " valor desde fortran"
-
-    print *, "Resultado:", entrada
-end program procesar_datos
